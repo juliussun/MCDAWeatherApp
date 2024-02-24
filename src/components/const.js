cd2e205fbc3efc6f0740f473ea84ddbdf5e1a082
Julius <test@test.com>
2024-02-23 23:09:15 -0400

    dev: use const to get description for wmo code

---
+++ src/components/const.js
@@ -0,0 +1,282 @@
+export const wmocode = {
+	"0":{
+		"day":{
+			"description":"Sunny",
+			"image":"weather-sunny"
+		},
+		"bgImg":{
+			"description":"Clear",
+			"image":require('../../assets/sunny.png')
+		}
+	},
+	"1":{
+		"day":{
+			"description":"Mainly Sunny",
+			"image":"weather-sunny"
+		},
+		"bgImg":{
+			"description":"Mainly Clear",
+			"image":require('../../assets/part_sunny.png')
+		}
+	},
+	"2":{
+		"day":{
+			"description":"Partly Cloudy",
+			"image":"weather-partly-cloudy"
+		},
+		"bgImg":{
+			"description":"Partly Cloudy",
+			"image":require('../../assets/cloudy.png')
+		}
+	},
+	"3":{
+		"day":{
+			"description":"Cloudy",
+			"image":"weather-partly-cloudy"
+		},
+		"bgImg":{
+			"description":"Cloudy",
+			"image":require('../../assets/cloudy.png')
+		}
+	},
+	"45":{
+		"day":{
+			"description":"Foggy",
+			"image":"weather-fog"
+		},
+		"bgImg":{
+			"description":"Foggy",
+			"image":require('../../assets/fog.png')
+		}
+	},
+	"48":{
+		"day":{
+			"description":"Rime Fog",
+			"image":"weather-fog"
+		},
+		"bgImg":{
+			"description":"Rime Fog",
+			"image":"http://openweathermap.org/img/wn/50n@2x.png"
+		}
+	},
+	"51":{
+		"day":{
+			"description":"Light Drizzle",
+			"image":"weather-rainy"
+		},
+		"bgImg":{
+			"description":"Light Drizzle",
+			"image":require('../../assets/man.jpg')
+		}
+	},
+	"53":{
+		"day":{
+			"description":"Drizzle",
+			"image":"weather-rainy"
+		},
+		"bgImg":{
+			"description":"Drizzle",
+			"image":require('../../assets/man.jpg')
+		}
+	},
+	"55":{
+		"day":{
+			"description":"Heavy Drizzle",
+			"image":"weather-rainy"
+		},
+		"bgImg":{
+			"description":"Heavy Drizzle",
+			"image":require('../../assets/man.jpg')
+		}
+	},
+	"56":{
+		"day":{
+			"description":"Light Freezing Drizzle",
+			"image":"weather-rainy"
+		},
+		"bgImg":{
+			"description":"Light Freezing Drizzle",
+			"image":require('../../assets/man.jpg')
+		}
+	},
+	"57":{
+		"day":{
+			"description":"Freezing Drizzle",
+			"image":"weather-rainy"
+		},
+		"bgImg":{
+			"description":"Freezing Drizzle",
+			"image":require('../../assets/man.jpg')
+		}
+	},
+	"61":{
+		"day":{
+			"description":"Light Rain",
+			"image":"weather-hail"
+		},
+		"bgImg":{
+			"description":"Light Rain",
+			"image":require('../../assets/man.jpg')
+		}
+	},
+	"63":{
+		"day":{
+			"description":"Rain",
+			"image":"weather-pouring"
+		},
+		"bgImg":{
+			"description":"Rain",
+			"image":require('../../assets/man.jpg')
+		}
+	},
+	"65":{
+		"day":{
+			"description":"Heavy Rain",
+			"image":"weather-pouring"
+		},
+		"bgImg":{
+			"description":"Heavy Rain",
+			"image":require('../../assets/man.jpg')
+		}
+	},
+	"66":{
+		"day":{
+			"description":"Light Freezing Rain",
+			"image":"weather-snowy-rainy"
+		},
+		"bgImg":{
+			"description":"Light Freezing Rain",
+			"image":require('../../assets/man.jpg')
+		}
+	},
+	"67":{
+		"day":{
+			"description":"Freezing Rain",
+			"image":"weather-snowy-rainy"
+		},
+		"bgImg":{
+			"description":"Freezing Rain",
+			"image":require('../../assets/man.jpg')
+		}
+	},
+	"71":{
+		"day":{
+			"description":"Light Snow",
+			"image":"weather-snowy"
+		},
+		"bgImg":{
+			"description":"Light Snow",
+			"image":require('../../assets/snow.png')
+		}
+	},
+	"73":{
+		"day":{
+			"description":"Snow",
+			"image":"weather-snowy"
+		},
+		"bgImg":{
+			"description":"Snow",
+			"image":require('../../assets/snow.png')
+		}
+	},
+	"75":{
+		"day":{
+			"description":"Heavy Snow",
+			"image":"weather-snowy-heavy"
+		},
+		"bgImg":{
+			"description":"Heavy Snow",
+			"image":require('../../assets/snow.png')
+		}
+	},
+	"77":{
+		"day":{
+			"description":"Snow Grains",
+			"image":"weather-snowy-heavy"
+		},
+		"bgImg":{
+			"description":"Snow Grains",
+			"image":require('../../assets/snow.png')
+		}
+	},
+	"80":{
+		"day":{
+			"description":"Light Showers",
+			"image":"weather-lightning-rainy"
+		},
+		"bgImg":{
+			"description":"Light Showers",
+			"image":require('../../assets/man.jpg')
+		}
+	},
+	"81":{
+		"day":{
+			"description":"Showers",
+			"image":"weather-lightning-rainy"
+		},
+		"bgImg":{
+			"description":"Showers",
+			"image":require('../../assets/man.jpg')
+		}
+	},
+	"82":{
+		"day":{
+			"description":"Heavy Showers",
+			"image":"weather-lightning-rainy"
+		},
+		"bgImg":{
+			"description":"Heavy Showers",
+			"image":require('../../assets/man.jpg')
+		}
+	},
+	"85":{
+		"day":{
+			"description":"Light Snow Showers",
+			"image":"weather-lightning-rainy"
+		},
+		"bgImg":{
+			"description":"Light Snow Showers",
+			"image":require('../../assets/man.jpg')
+		}
+	},
+	"86":{
+		"day":{
+			"description":"Snow Showers",
+			"image":"weather-snowy-rainy"
+		},
+		"bgImg":{
+			"description":"Snow Showers",
+			"image":require('../../assets/man.jpg')
+		}
+	},
+	"95":{
+		"day":{
+			"description":"Thunderstorm",
+			"image":"weather-lightning-rainy"
+		},
+		"bgImg":{
+			"description":"Thunderstorm",
+			"image":require('../../assets/man.jpg')
+		}
+	},
+	"96":{
+		"day":{
+			"description":"Light Thunderstorms With Hail",
+			"image":"weather-lightning-rainy"
+		},
+		"bgImg":{
+			"description":"Light Thunderstorms With Hail",
+			"image":require('../../assets/man.jpg')
+		}
+	},
+	"99":{
+		"day":{
+			"description":"Thunderstorm With Hail",
+			"image":"weather-lightning-rainy"
+		},
+		"bgImg":{
+			"description":"Thunderstorm With Hail",
+			"image":require('../../assets/man.jpg')
+		}
+	}
+}
